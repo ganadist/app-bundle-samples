a380570a58088b0a4784a00608530d219b0f1d52
YOUNG HO CHA <ganachoco@linecorp.com>
2020-05-22 16:17:40 +0900

    Convert build.gradle to build.gradle.kts

--- DynamicFeatures/build.gradle.kts
+++ DynamicFeatures/build.gradle.kts
@@ -17,23 +17,24 @@
 // Top-level build file where you can add configuration options common to all sub-projects/modules.
 
 buildscript {
+    //extra.versions =
+    val kotlinVersion = "1.3.61"
+    extra.set("versions" , mapOf(
+            "compileSdk"        to 28,
+            "minSdk"            to 24,
+            "targetSdk"         to 28,
+            "kotlin"            to kotlinVersion,
+            "playcore"          to "1.6.4",
+            "appcompat"         to "1.1.0",
+            "espresso"          to "3.2.0",
+            "extJunit"          to "1.1.1",
+            "testRules"         to "1.2.0",
+            "testRunner"        to "1.2.0"
+    ))
 
-    ext.versions = [
-            'compileSdk'        : 28,
-            'minSdk'            : 24,
-            'targetSdk'         : 28,
-            'kotlin'            : '1.3.61',
-            'playcore'          : '1.6.4',
-            'appcompat'         : '1.1.0',
-            'espresso'          : '3.2.0',
-            'extJunit'          : '1.1.1',
-            'testRules'         : '1.2.0',
-            'testRunner'        : '1.2.0',
-    ]
-
-    ext.names = [
-            'applicationId': 'com.google.android.samples.dynamicfeatures.ondemand'
-    ]
+    extra.set("names",  mapOf(
+            "applicationId" to "com.google.android.samples.dynamicfeatures.ondemand"
+    ))
 
     repositories {
         google()
@@ -41,8 +42,8 @@ buildscript {
     }
 
     dependencies {
-        classpath 'com.android.tools.build:gradle:4.0.0-rc01'
-        classpath "org.jetbrains.kotlin:kotlin-gradle-plugin:${versions.kotlin}"
+        classpath("com.android.tools.build:gradle:4.0.0-rc01")
+        classpath("org.jetbrains.kotlin:kotlin-gradle-plugin:$kotlinVersion")
 
         // NOTE: Do not place your application dependencies here; they belong
         // in the individual module build.gradle files
